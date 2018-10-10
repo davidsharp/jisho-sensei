1991ea263a165f5a52db48e13b0cfd922585022d
David Sharp <davidsharp@charanga.com>
2018-10-10 15:57:23 +0100

    neaten up

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# yonde(hanashite) kudasai!
+# jisho-san, yonde(hanashite) kudasai!
 
-This is a WIP Chrome extension that reads some Japanese
+This is a WIP Chrome extension that reads some Japanese from jisho.org
 
 ### To use:
 This is in no way really any use, but if you want to give it a go, pull the repo open `chrome://extensions` and then `Load unpacked extension...`.
 
-To _actually_ use it, highlight a word or kanji and then press <kbd>Ctrl+Shift+Y</kbd>. Alternatively, you can also type `yonde` into your address bar, hit space and then type your word.
+To _actually_ use it, navigate to jisho.org, search for a word, and then hit the 🗣 to a word or sentence being read!
--- inject.js
+++ inject.js
@@ -1,23 +1,5 @@
 // find places where japanese is, ignore furigana, and read when speaker emoji is clicked
 
-/*var selection = ''
-var time = undefined
-var throttle = 300
-
- document.onselectionchange=function(){
-   if(time)clearTimeout(time)
-   time=setTimeout(messageExtension,throttle)
- }
-
-
-function messageExtension(){
-  console.log(window.getSelection())
-  if(window.getSelection().toString()!==selection)chrome.extension.sendMessage(window.getSelection().toString(), function(response) {
-     //callback
-
-  });
-}*/
-
 if(/jisho\.org/gi.test(location.hostname)){
   var japanese = document.querySelectorAll('.japanese')
   //japanese.forEach(c=>console.log(c.innerText))
@@ -25,11 +7,11 @@ if(/jisho\.org/gi.test(location.hostname)){
   var noFurigana = []
   japanese.forEach(c=>noFurigana.push(c.querySelectorAll(':not(.furigana)')))
   noFurigana.forEach(c=>c.forEach(_c=>{
-    //console.log(_c/*.innerText*/)
     var speaker = document.createElement('div')
     speaker.onclick=function(){yonde(_c.innerText)}
     speaker.innerText="🗣"
     _c.appendChild(speaker)
+    console.log(_c)
   }))
 }
 
